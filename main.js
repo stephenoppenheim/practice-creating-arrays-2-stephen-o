a30d7729f3402e25ce7269a981c88eacb5a76e18
stephenoppenheim <sjocoding@gmail.com>
2026-05-04 10:47:03 -0500

    // Create arrays according to challenge requirements     - Initialize and populate a seven element array of "Hello" strings using fill, and log it to the console.     - Update array and switch elements 0, 3 and 6 to "Hi" and log it to the console     - Initialize a five element array and fill it with i * 10 using a for loop, and log it to the console.

---
+++ main.js
@@ -0,0 +1,15 @@
+
+// Initialize array
+let helloArray = Array(7).fill("Hello");
+console.log(helloArray.length === 7, helloArray);
+
+// Update array
+helloArray[0] = "Hi";
+helloArray[3] = "Hi";
+helloArray[6] = "Hi";
+console.log(helloArray.length, helloArray);
+
+// Create and populate array with for loop
+const indexTimesTen = Array(5);
+for (let i = 0; i < 5; i++) indexTimesTen[i] = i * 10;
+console.log(indexTimesTen.length, indexTimesTen);
